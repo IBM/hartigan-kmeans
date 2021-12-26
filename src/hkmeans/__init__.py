baadaa2fa01bd54329383b6517fed463c5f523da
ASSAF TOLEDO <Assaf.Toledo@ibm.com>
2021-12-27 00:02:24 +0200

    rollback to versioneer 0.19

--- src/hkmeans/__init__.py
+++ src/hkmeans/__init__.py
@@ -15,5 +15,6 @@ import hkmeans.c_package  # cython with cpp version
 # import core functionality
 from hkmeans import *
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

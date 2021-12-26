46fbb9718e808c3eab2d612a1db685a2a94aa30d
ASSAF TOLEDO <Assaf.Toledo@ibm.com>
2021-12-26 13:28:33 +0200

    changes to versioneer setup

--- src/hkmeans/_version.py
+++ src/hkmeans/_version.py
@@ -42,8 +42,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "'v'"
-    cfg.parentdir_prefix = "hkmeans-"
+    cfg.tag_prefix = "v"
+    cfg.parentdir_prefix = "hartigan-kmeans-"
     cfg.versionfile_source = "src/hkmeans/_version.py"
     cfg.verbose = False
     return cfg
